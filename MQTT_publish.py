f661e752c21c6c55b200d5350f837367eae3503d
Jason Eisele <jmeisele@yahoo.com>
2018-01-13 20:34:26 -0600

    Update MQTT_publish.py

--- MQTT_publish.py
+++ MQTT_publish.py
@@ -3,6 +3,6 @@
 
 import paho.mqtt.publish as publish
 
-publish.single("Home/Temperature/Living_room", "70", hostname = "test.morquitto.org)
+publish.single("Home/Temperature/Family_room", "70", hostname = "test.morquitto.org)
 publish.single("Home/Temperature/Living_room", "72", hostname = "test.mosquitto.org")
 print("Done")
