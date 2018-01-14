d2fe53dde51788d389a76317ce392b77c7b4a498
Jason Eisele <jmeisele@yahoo.com>
2018-01-13 19:48:59 -0600

    Create MQTT_publish.py

---
+++ MQTT_publish.py
@@ -0,0 +1,8 @@
+# MQTT Publish Demo
+# Publish two messages to two different topics
+
+import paho.mqtt.publish as publish
+
+publish.single("Home/Temperature/Living_room", "70", hostname = "test.morquitto.org)
+publish.single("Home/Temperature/Living_room", "72", hostname = "test.mosquitto.org")
+print("Done")
