9443319eebad5d3ee318335a9c18f8882a5ff62c
Jason Eisele <jmeisele@yahoo.com>
2018-01-13 20:34:53 -0600

    Update MQTT_client.py

--- MQTT_client.py
+++ MQTT_client.py
@@ -6,8 +6,8 @@ def on_connect(client, userdata, flags, rc):
 
     # Subscribing in on_connect() means that if we lose the connection and
     # reconnect then subscriptions will be renewed.
-    client.subscribe("IoT/test")
-    client.subscribe("DataScience/test")
+    client.subscribe("Home/Temperature/Family_room")
+    client.subscribe("Home/Temperature/Living_room")
 
 # The callback for when a PUBLISH message is received from the server.
 def on_message(client, userdata, msg):
