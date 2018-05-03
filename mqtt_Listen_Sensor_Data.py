a8e0b78e94ac258266aa9bab82dba68ea1760843
Jason Eisele <jmeisele@yahoo.com>
2018-05-03 08:53:00 -0500

    Update mqtt_Listen_Sensor_Data.py

--- mqtt_Listen_Sensor_Data.py
+++ mqtt_Listen_Sensor_Data.py
@@ -1,11 +1,3 @@
-#------------------------------------------
-#--- Author: Pradeep Singh
-#--- Date: 20th January 2017
-#--- Version: 1.0
-#--- Python Ver: 2.7
-#--- Details At: https://iotbytes.wordpress.com/store-mqtt-data-from-sensors-into-sql-database/
-#------------------------------------------
-
 import paho.mqtt.client as mqtt
 from store_Sensor_Data_to_DB import sensor_Data_Handler
 
@@ -16,16 +8,16 @@ Keep_Alive_Interval = 45
 MQTT_Topic = "Home/BedRoom/#"
 
 #Subscribe to all Sensors at Base Topic
-def on_connect(mosq, obj, rc):
+def on_connect(mosq, obj, flags, rc):
 	mqttc.subscribe(MQTT_Topic, 0)
 
 #Save Data into DB Table
 def on_message(mosq, obj, msg):
 	# This is the Master Call for saving MQTT Data into DB
 	# For details of "sensor_Data_Handler" function please refer "sensor_data_to_db.py"
-	print "MQTT Data Received..."
-	print "MQTT Topic: " + msg.topic  
-	print "Data: " + msg.payload
+	print ("MQTT Data Received...")
+	print ("MQTT Topic: " + str(msg.topic))
+	print ("Data: " + str(msg.payload))
 	sensor_Data_Handler(msg.topic, msg.payload)
 
 def on_subscribe(mosq, obj, mid, granted_qos):
